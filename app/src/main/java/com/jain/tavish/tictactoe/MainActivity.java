86af1b898f7d14cfeda7e99dc2b601165ce32e80
tavishjain <jaintavish@yahoo.com>
2018-10-01 21:38:31 +0530

    Basic Application Set Up

---
+++ app/src/main/java/com/jain/tavish/tictactoe/MainActivity.java
@@ -0,0 +1,236 @@
+package com.jain.tavish.tictactoe;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public ImageView imageView1 , imageView2 , imageView3 , imageView4 , imageView5 , imageView6 , imageView7 , imageView8 , imageView9;
+    public boolean cross;
+    public TextView turn_teller;
+    public int[] cross_number , circle_number;
+    public int steps_count;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        turn_teller = findViewById(R.id.tv_turn_teller);
+
+        imageView1 = findViewById(R.id.id_0_0);
+        imageView2 = findViewById(R.id.id_0_1);
+        imageView3 = findViewById(R.id.id_0_2);
+        imageView4 = findViewById(R.id.id_1_0);
+        imageView5 = findViewById(R.id.id_1_1);
+        imageView6 = findViewById(R.id.id_1_2);
+        imageView7 = findViewById(R.id.id_2_0);
+        imageView8 = findViewById(R.id.id_2_1);
+        imageView9 = findViewById(R.id.id_2_2);
+
+        new_game();
+
+        imageView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((ImageView) view).setImageResource(getImageResourceForView(1));
+                imageView1.setEnabled(false);
+            }
+        });
+
+        imageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((ImageView) view).setImageResource(getImageResourceForView(2));
+                imageView2.setEnabled(false);
+            }
+        });
+
+        imageView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((ImageView) view).setImageResource(getImageResourceForView(3));
+                imageView3.setEnabled(false);
+            }
+        });
+
+        imageView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((ImageView) view).setImageResource(getImageResourceForView(4));
+                imageView4.setEnabled(false);
+            }
+        });
+
+        imageView5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((ImageView) view).setImageResource(getImageResourceForView(5));
+                imageView5.setEnabled(false);
+            }
+        });
+
+        imageView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((ImageView) view).setImageResource(getImageResourceForView(6));
+                imageView6.setEnabled(false);
+            }
+        });
+
+        imageView7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((ImageView) view).setImageResource(getImageResourceForView(7));
+                imageView7.setEnabled(false);
+            }
+        });
+
+        imageView8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((ImageView) view).setImageResource(getImageResourceForView(8));
+                imageView8.setEnabled(false);
+            }
+        });
+
+        imageView9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((ImageView) view).setImageResource(getImageResourceForView(9));
+                imageView9.setEnabled(false);
+            }
+        });
+
+    }
+
+    public int pxToDp(int px) {
+        DisplayMetrics displayMetrics = getApplicationContext().getResources().getDisplayMetrics();
+        return Math.round(px / (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+    }
+
+    public int getImageResourceForView(int iv_id){
+        int id = R.drawable.ic_cross_yellow_red_back;
+
+        steps_count++;
+
+        if(cross == false){ //Code for O
+            cross = true;
+            id = R.drawable.ic_cross_yellow_red_back;
+            circle_number[iv_id - 1] = 0;
+            turn_teller.setText("O's turn");
+            checkForWin(circle_number , 'X');
+        }else if(cross == true){ //Code for X
+            cross = false;
+            cross_number[iv_id - 1] = 0;
+            id = R.drawable.ic_circle_yellow_red_back;
+            checkForWin(cross_number , 'O');
+            turn_teller.setText("X's turn");
+        }
+
+        return id;
+    }
+//player number of O is 0 and for X is 1
+
+    public void checkForWin(int[] array , char player){
+        if(array[0] == 0 && array[1] == 0 && array[2] == 0){
+            playerWon(player);
+        }else  if(array[3] == 0 && array[4] == 0 && array[5] == 0){
+            playerWon(player);
+        }else  if(array[6] == 0 && array[7] == 0 && array[8] == 0){
+            playerWon(player);
+        }else  if(array[0] == 0 && array[3] == 0 && array[6] == 0){
+            playerWon(player);
+        }else  if(array[1] == 0 && array[4] == 0 && array[7] == 0){
+            playerWon(player);
+        }else  if(array[2] == 0 && array[5] == 0 && array[8] == 0){
+            playerWon(player);
+        }else  if(array[0] == 0 && array[4] == 0 && array[8] == 0){
+            playerWon(player);
+        }else  if(array[2] == 0 && array[4] == 0 && array[6] == 0){
+            playerWon(player);
+        }else if(steps_count == 9){
+            playerWon('a');
+        }
+    }
+
+    public void playerWon(char winner){
+
+        imageView1.setEnabled(false);
+        imageView2.setEnabled(false);
+        imageView3.setEnabled(false);
+        imageView4.setEnabled(false);
+        imageView5.setEnabled(false);
+        imageView6.setEnabled(false);
+        imageView7.setEnabled(false);
+        imageView8.setEnabled(false);
+        imageView9.setEnabled(false);
+
+        AlertDialog.Builder builder;
+        builder = new AlertDialog.Builder(this);
+        if(winner == 'X'){
+            builder.setTitle("X Wins !!!!!");
+        }else if(winner == 'O'){
+            builder.setTitle("O Wins !!!!!");
+        }else if(winner == 'a'){
+            builder.setTitle("Match Drawn !!");
+        }
+        builder.setPositiveButton("New Game", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        new_game();
+                    }
+                })
+                .show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu , menu);
+        return true;
+    }
+
+    public void new_game(){
+
+        steps_count = 0 ;
+
+        imageView1.setEnabled(true);
+        imageView2.setEnabled(true);
+        imageView3.setEnabled(true);
+        imageView4.setEnabled(true);
+        imageView5.setEnabled(true);
+        imageView6.setEnabled(true);
+        imageView7.setEnabled(true);
+        imageView8.setEnabled(true);
+        imageView9.setEnabled(true);
+
+        cross_number = new int[]{-1, -1, -1, -1, -1, -1, -1, -1, -1};
+        circle_number = new int[]{-1, -1, -1, -1, -1, -1, -1, -1, -1};
+
+        imageView1.setImageResource(0);
+        imageView2.setImageResource(0);
+        imageView3.setImageResource(0);
+        imageView4.setImageResource(0);
+        imageView5.setImageResource(0);
+        imageView6.setImageResource(0);
+        imageView7.setImageResource(0);
+        imageView8.setImageResource(0);
+        imageView9.setImageResource(0);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.new_game){
+            new_game();
+        }
+        return true;
+    }
+}
